0fcabb37e31800ae557bff360a35a27148d5ce8d
pponworks <pponworks@gmail.com>
2026-07-05 16:35:20 +0700

    feat: add /game/start router

--- server.ts
+++ server.ts
@@ -6,7 +6,12 @@ app.use(express.json());
  
 const games: Record<string, Game> = {};
 
-app.post('/game/start', (req: Request, res: Response) => {});
+app.post('/game/start', (req: Request, res: Response) => {
+    let balance: number = req.body.initial_balance ?? 1000;
+    let game = new Game(balance);
+    games[game.id] = game;
+    res.json(game.view());
+});
 
 app.post('/game/:game_id/action', (req: Request, res: Response) => {});
 
