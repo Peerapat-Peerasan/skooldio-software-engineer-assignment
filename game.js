1df5fe300ff4335fbf127a4651363dd970c5a1b4
pponworks <pponworks@gmail.com>
2026-07-05 14:58:03 +0700

    feat: add shuffle and cutDeck function

--- game.js
+++ game.js
@@ -13,6 +13,18 @@ function makeDeck() {
     return cards
 }
 
+function shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
+}
+
+function cutDeck(deck, amount){
+    return deck.slice(n).concat(deck.slice(0, n));
+}
+
 class Game {
     constructor() {
         this.id = crypto.randomUUID;
