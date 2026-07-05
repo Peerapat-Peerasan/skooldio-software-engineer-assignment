49acae30c1a33ea17748026460af368c964a3d82
pponworks <pponworks@gmail.com>
2026-07-05 14:46:00 +0700

    feat: add makeDeck function

--- game.js
+++ game.js
@@ -1,5 +1,18 @@
 const crypto = require('crypto');
 
+const SUITS = ['S', 'H', 'D', 'C'];
+const RANKS = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+
+function makeDeck() {
+    let cards = [];
+    for (let s of SUITS) {
+        for (let r of RANKS){
+            cards.push({suit: s, rank: r});
+        }
+    }
+    return cards
+}
+
 class Game {
     constructor() {
         this.id = crypto.randomUUID;
